6e4abb54a79f9117560df174fc4701f50a6d0c5a
Muhammad Usama <66507968+Muhammad-Usama-1@users.noreply.github.com>
2022-11-18 21:56:13 +0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.237.239.211:9000/',
+  serverUrl: 'http://44.195.82.245:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
